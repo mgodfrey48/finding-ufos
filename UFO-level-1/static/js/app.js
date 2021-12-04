c138929419d99b745250ed311ae9f144de10b360
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-12-04 13:57:44 -0600

    Added an event handler to call a function that clears the html table and writes a new one filtering the data to include only the date the user input.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,11 +4,6 @@ console.log("app.js running")
 // from data.js
 var tableData = data;
 
-// Store the dates in an array
-var dates = tableData.map(sighting => {
-    return sighting.datetime;
-});
-
 // Create a table body object
 var tbody = d3.select("tbody");
 
@@ -20,6 +15,43 @@ tableData.forEach(sighting => {
         // Append a cell to the row for each value in the [key, value] pairings
         var cell = row.append("td");
         cell.text(value);
-    })
-})
+    });
+});
 
+console.log(tbody)
+
+// Render data fitered on user input
+var form = d3.select("form");
+
+form.on("submit", runEnter);
+
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Print the value to the console
+    console.log(inputValue);
+
+    // Store all of the sightings from the date that matches the input data
+    var dateMatch = tableData.filter(sighting => sighting.datetime === inputValue)
+    
+    // Clear the html for the table to be replaced with filtered data
+    tbody.html("");
+
+    // Display the filtered sightings in the cleared table
+    dateMatch.forEach(sighting => {
+        var row = tbody.append("tr");
+        // Loop through each [key, value] pair in the data entry
+        Object.entries(sighting).forEach(([key, value]) => {
+            // Append a cell to the row for each value in the [key, value] pairings
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
