3b6b2636af423dec6d5f77b0e394f41e98524da9
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-12-06 17:34:21 -0600

    Added the image and css files to the static folder in UFO-level2. Added input and label tags to index_level2.html.

---
+++ UFO-level-2/static/js/app_level2.js
@@ -0,0 +1,61 @@
+// Check to see if app.js is running
+console.log("app_level2.js running")
+
+// from data.js
+var tableData = data;
+
+// Create a table body object
+var tbody = d3.select("tbody");
+
+// Loop through the UFO data and use d3 
+tableData.forEach(sighting => {
+    var row = tbody.append("tr");
+    // Loop through each [key, value] pair in the data entry
+    Object.entries(sighting).forEach(([key, value]) => {
+        // Append a cell to the row for each value in the [key, value] pairings
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Render data fitered on user input
+var form = d3.select("form");
+
+form.on("submit", runEnter);
+
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input elements and get the raw HTML node
+    var dateElement = d3.select("#datetime");
+    // var cityElement = d3.select("#city");
+    // var stateElement = d3.select("#state");
+    // var shapeElement = d3.select("#shape");
+    // var durationElement = d3.select("#duration");
+
+    // Get the value property of the input element
+    var dateValue = dateElement.property("value");
+    // var cityValue = cityElement.property("value");
+
+    // Print the value to the console
+    console.log(dateValue);
+    // console.log(cityValue);
+
+    // Store all of the sightings from the date that matches the input data
+    // var dateMatch = tableData.filter(sighting => sighting.datetime === inputValue)
+    
+    // // Clear the html for the table to be replaced with filtered data
+    // tbody.html("");
+
+    // // Display the filtered sightings in the cleared table
+    // dateMatch.forEach(sighting => {
+    //     var row = tbody.append("tr");
+    //     // Loop through each [key, value] pair in the data entry
+    //     Object.entries(sighting).forEach(([key, value]) => {
+    //         // Append a cell to the row for each value in the [key, value] pairings
+    //         var cell = row.append("td");
+    //         cell.text(value);
+    //     });
+    // });
+};
