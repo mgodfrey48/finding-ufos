03fc6e705d1cae3a735bc6fc2245a4c0b60c5ad6
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2021-12-06 21:19:23 -0600

    Added the rest of the filters to the index_level2.html page and successfully stored the input data.

--- UFO-level-2/static/js/app_level2.js
+++ UFO-level-2/static/js/app_level2.js
@@ -21,26 +21,34 @@ tableData.forEach(sighting => {
 // Render data fitered on user input
 var form = d3.select("form");
 
-form.on("submit", runEnter);
+form.on("change", runEnter);
 
 function runEnter() {
+    console.log('runEnter running');
     // Prevent the page from refreshing
     d3.event.preventDefault();
     
     // Select the input elements and get the raw HTML node
     var dateElement = d3.select("#datetime");
-    // var cityElement = d3.select("#city");
-    // var stateElement = d3.select("#state");
-    // var shapeElement = d3.select("#shape");
-    // var durationElement = d3.select("#duration");
+    var cityElement = d3.select("#city");
+    var stateElement = d3.select("#state");
+    var shapeElement = d3.select("#shape");
+    var durationElement = d3.select("#duration");
 
     // Get the value property of the input element
     var dateValue = dateElement.property("value");
-    // var cityValue = cityElement.property("value");
+    var cityValue = cityElement.property("value");
+    var stateValue = stateElement.property("value");
+    var shapeValue = shapeElement.property("value");
+    var durationValue = durationElement.property("value");
 
     // Print the value to the console
     console.log(dateValue);
-    // console.log(cityValue);
+    console.log(cityValue);
+    console.log(stateValue);
+    console.log(shapeValue);
+    console.log(durationValue);
+
 
     // Store all of the sightings from the date that matches the input data
     // var dateMatch = tableData.filter(sighting => sighting.datetime === inputValue)
